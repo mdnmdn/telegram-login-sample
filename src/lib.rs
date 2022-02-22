e7cf465b8ad81830feadf63c1d85575c4c958c7e
Marco De Nittis <marco.denittis@gmail.com>
2022-02-22 23:44:09 +0100

    fix url webhook typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use actix_web::{HttpRequest, Responder, HttpResponse, post};
 
 
-#[post("/bot_hoot_221")]
+#[post("/bot_hook_221")]
 pub async fn bot_web_hook(_req: HttpRequest) -> impl Responder {
   HttpResponse::Ok()
 }
