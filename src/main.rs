c84bee7448615c13f0745ce99a3007e94978748c
Marco De Nittis <marco.denittis@gmail.com>
2022-02-22 22:42:08 +0100

    working sample

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-use actix_web::{web, App, HttpResponse, HttpServer, middleware::Logger};
+use actix_web::{web, App, HttpResponse, HttpServer, middleware::Logger, HttpRequest};
 use actix_files::Files;
 use env_logger::Env;
 
 #[actix_web::main()]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let port: i32 = std::env::var("PORT")
         .unwrap_or_default()
         .parse()
         .unwrap_or(4050);
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let binding = format!("0.0.0.0:{port}");
 
-    log::info!("starting...");
+    log::info!("starting on {binding}...");
 
     HttpServer::new(move || {
         App::new()
           .wrap(Logger::default())
-          .service(Files::new("/", "/static").index_file("index.html"))
-          .route("/wow", web::to(|| HttpResponse::Ok().body("wow!")))
+          .route("/wow", web::get().to(move || async { HttpResponse::Ok().body("wow!") }))
+          .service(Files::new("/", "./static").index_file("index.html"))
     })
-    .bind(format!("0.0.0.0:{}", port))?
+    .bind(binding)?
     .workers(2)
     .run()
     .await?;
